2ad5934dad9dcbe5d108f9978e42145708d10a54
Seremba <pserembae@gmail.com>
2022-01-19 16:06:40 +0300

    Java Data Structures and Algorithms

---
+++ src/sorting/BubbleSorting.java
@@ -0,0 +1,36 @@
+package sorting;
+
+import java.util.Arrays;
+
+public class BubbleSorting {
+
+	public static void main(String[] args) {
+		int[] arr = {3, 1, 5, 4, 2};
+		bubble(arr);
+		System.out.println(Arrays.toString(arr));
+	}
+
+	static void bubble(int[] arr){
+		boolean swapped;
+		// run the steps n-1 times
+		for (int i = 0; i < arr.length; i++) {
+			swapped = false;
+			// for each step the max item will come at the last respective index
+			for (int j = 1; j < arr.length- i; j++) {
+				// swap if the item is smaller than the prev item
+				if(arr[j] < arr[j-1]){
+					// swap
+					int temp = arr[j];
+					arr[j] = arr[j-1];
+					arr[j-1] = temp;
+					swapped = true;
+				}
+			}
+//			if you did not swap for a particular value of i, it means the array is already sorted therefore stop the program
+			if(!swapped){
+				break;
+			}
+		}
+	}
+
+}
